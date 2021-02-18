e0cb02bda875230b78dde968f7aca036a2d6ac15
lulumuts <lulumutuli24@gmail.com>
2021-02-18 14:35:00 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,2 @@
+//TODO: ADD SOURCEMAP SUPPORT
+required("./build/server")
---
+++ src/server/server.js
@@ -0,0 +1,6 @@
+const bleugh ="whoa";
+console.log(`hello ${bleugh}`);
+
+const obj = {hey:1};
+const obj2 ={...obj, bleugh:2};
+console.log(obj2);
